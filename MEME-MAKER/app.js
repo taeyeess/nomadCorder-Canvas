5f8bc8f06db71a26aa8448e3caf85e571d857039
taeyee <taeyeeya@gmail.com>
2022-09-27 15:42:55 +0900

    fillRect, strokeRect, moveTo, lineTo, arc 집만들기

---
+++ MEME-MAKER/app.js
@@ -0,0 +1,45 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 800;
+// ctx.lineWidth = 2;
+
+// ctx.fillRect(50, 50, 100, 100);
+// ctx.strokeRect(150, 150, 100, 100);
+// ctx.fillRect(250, 250, 100, 100);
+// ctx.fillStyle = 'orange';
+// ctx.fillRect(350, 350, 100, 100);
+
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.fillStyle = 'orange';
+// ctx.fill();
+// ctx.beginPath();
+// ctx.rect(250, 250, 100, 100);
+// ctx.stroke();
+
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.strokeStyle = 'blue';
+
+// ctx.stroke();
+// ctx.fillStyle = 'red';
+// ctx.fill();
+
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(600, 200, 50, 200);
+ctx.strokeRect(380, 300, 100, 100);
+ctx.arc(400, 350, 10, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(150, 200);
+ctx.lineTo(700, 200);
+ctx.lineTo(420, 50);
+ctx.lineTo(150, 200);
+ctx.fillStyle = 'red';
+ctx.fill();
+
