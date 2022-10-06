1f6b8ed7b9cd6466c6c7449bacd87132d0b82d95
taeyee <taeyeeya@gmail.com>
2022-10-06 17:27:01 +0900

    Array.from(), forEach(), Fillmode, destroy, erase button

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,8 +1,17 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const eraseBtn = document.getElementById('eraser-btn');
+const destroyBtn = document.getElementById('destroy-btn');
+const modeBtn = document.getElementById('mode-btn');
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-// ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 // ctx.fillRect(50, 50, 100, 100);
 // ctx.strokeRect(150, 150, 100, 100);
@@ -45,30 +54,94 @@ canvas.height = 800;
 // ctx.fill();
 
 let isPainting = false;
+let isFilling = false;
 
-function onMove(event){ 
-  if(isPainting){
+function onMove(event) {
+  if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
   }
   ctx.moveTo(event.offsetX, event.offsetY);
+  ctx.beginPath();
   // if(isPainting){
   //   console.log(event.offsetX, event.offsetY);
-  //   ctx.lineTo(event.offsetX, event.offsetY);  
+  //   ctx.lineTo(event.offsetX, event.offsetY);
   //   ctx.stroke();
   // }
   // ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function onMouseDown(){
+function onMouseDown() {
   isPainting = true;
 }
 
-function onMouseUp(){
+function onMouseUp() {
   isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  // console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  //console.log(event.target.value);
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+  const colorValue = event.target.dataset.color;
+  console.dir(event.target.dataset.color);
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue ;
+}
+
+function onModeChange(){
+  if(isFilling){
+    isFilling = false;
+    // ctx.fillRect(0, 0, 800, 800); 내가 처음에 작성한 코드
+    modeBtn.innerText = 'Fill';
+    
+  }else{
+    isFilling = true;
+    modeBtn.innerText = 'Draw';
+  }
+}
+
+function onCanvasClick(){
+  if(isFilling){
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
+function onDestroy(){
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 800, 800);
+}
+
+function onErase(){
+  ctx.strokeStyle = 'white';
+  isFilling = false;
+  modeBtn.innerText = 'Fill';
 }
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
+
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+// console.log(colorOptions); -> HTMLCollection
+colorOptions.forEach((color) => {
+  color.addEventListener("click", onColorClick);
+});
+
+modeBtn.addEventListener("click", onModeChange);
+destroyBtn.addEventListener("click", onDestroy);
+eraseBtn.addEventListener("click", onErase);
